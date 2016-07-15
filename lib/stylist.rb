d5883993c873d4e44b3a2eb23fb0c2d0b78b003e
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-15 10:56:35 -0700

    add client method to stylist class

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -44,4 +44,15 @@ class Stylist
     @name = attributes.fetch(:name, @name)
     DB.exec("UPDATE stylists SET name = '#{@name}' WHERE id = #{self.id()}")
   end
+
+  define_method(:clients) do
+    stylist_clients = []
+    clients = DB.exec("SELECT * FROM clients WHERE stylist_id = #{self.id()};")
+    clients.each() do |client|
+      name = client.fetch("name")
+      stylist_id = client.fetch("stylist_id").to_i()
+      stylist_clients.push(Client.new({:name => name, :stylist_id => stylist_id}))
+    end
+    stylist_clients
+end
 end
