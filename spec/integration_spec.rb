8e11cad8c60c6ea2a04f117ad791778092debcdf
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-15 13:40:33 -0700

    update integration spec for renaming client, passing

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,21 +1,21 @@
-require('capybara/rspec')
-require('./app')
-require('launchy')
+require("capybara/rspec")
+require("./app")
+require("launchy")
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('adding a new stylist', {:type => :feature}) do
-  it('allows a user to add a new stylist') do
-    visit('/')
-    fill_in('stylist_name', :with => "Gerald")
-    click_button('Add Stylist')
-    expect(page).to have_content('Success!')
-    click_link('Return Home')
+describe("adding a new stylist", {:type => :feature}) do
+  it("allows a user to add a new stylist") do
+    visit("/")
+    fill_in("stylist_name", :with => "Gerald")
+    click_button("Add Stylist")
+    expect(page).to have_content("Success!")
+    click_link("Return Home")
     expect(page).to have_content("Gerald")
   end
 end
 
-describe('deleting a stylist', {:type => :feature}) do
+describe("deleting a stylist", {:type => :feature}) do
   it "allows a user to delete a stylist" do
     gerry = Stylist.new({:name => "Gerry", :id => nil})
     gerry.save()
@@ -27,7 +27,7 @@ describe('deleting a stylist', {:type => :feature}) do
   end
 end
 
-describe('renaming a stylist', {:type => :feature}) do
+describe("renaming a stylist", {:type => :feature}) do
   it "allows a user to rename a stylist" do
     gerry = Stylist.new({:name => "Gerry", :id => nil})
     gerry.save()
@@ -40,7 +40,7 @@ describe('renaming a stylist', {:type => :feature}) do
   end
 end
 
-describe('add a client to a stylist', {:type => :feature}) do
+describe("add a client to a stylist", {:type => :feature}) do
   it "allows a user to add a client to a stylist" do
     gerry = Stylist.new({:name => "Gerry", :id => nil})
     gerry.save()
@@ -54,7 +54,7 @@ describe('add a client to a stylist', {:type => :feature}) do
   end
 end
 
-describe('remove a client', {:type => :feature}) do
+describe("remove a client", {:type => :feature}) do
   it "allows a user to remove a client" do
     gerry = Stylist.new({:name => "Gerry", :id => nil})
     gerry.save()
@@ -63,8 +63,26 @@ describe('remove a client', {:type => :feature}) do
     fill_in("name", :with => "Eleanore")
     click_button("Add client")
     click_link("Return Home")
-    select('Eleanore', :from => "remove_client_id")
+    select("Eleanore", :from => "remove_client_id")
     click_button("Delete Client")
     expect(page).to have_content("Success")
+  end
+end
+
+describe("rename a client", {:type => :feature}) do
+  it "allows a user to rename a client" do
+    gerry = Stylist.new({:name => "Gerry", :id => nil})
+    gerry.save()
+    visit("/")
+    click_link("Gerry")
+    fill_in("name", :with => "Eleanore")
+    click_button("Add client")
+    click_link("Return Home")
+    select("Eleanore", :from => "rename_client_id")
+    fill_in("new_client_name", :with => "Bruce")
+    click_button("Rename Client")
+    click_link("Return Home")
+    click_link("Gerry")
+    expect(page).to have_content("Bruce")
   end
 end
