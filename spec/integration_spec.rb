f355fac914717d8092f3bfa9e81a774224669b9e
Caleb Stokka <caleb.stokka@gmail.com>
2016-07-15 12:03:13 -0700

    add integration spec for deleting client from stylist, passing

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -40,3 +40,19 @@ describe('add a client to a stylist', {:type => :feature}) do
     expect(page).to have_content("Eleanore")
   end
 end
+
+describe('remove a client from a stylist', {:type => :feature}) do
+  it "allows a user to add a client to a stylist" do
+    gerry = Stylist.new({:name => "Gerry", :id => nil})
+    gerry.save()
+    visit("/")
+    click_link("Gerry")
+    fill_in("name", :with => "Eleanore")
+    click_button("Add client")
+    click_link("Return Home")
+    click_link("Gerry")
+    expect(page).to have_content("Eleanore")
+    click_button("Delete Client")
+    expect(page).to have_content("Success")
+  end
+end
